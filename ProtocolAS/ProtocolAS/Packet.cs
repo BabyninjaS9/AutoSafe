1bec7183a536baea98131e4bc168ef100acc175f
Wesley Steenbergen <39294044+Wesley2712@users.noreply.github.com>
2018-05-24 15:23:55 +0200

    Protocol Updated, User Tests succeeds

--- ProtocolAS/ProtocolAS/Packet.cs
+++ ProtocolAS/ProtocolAS/Packet.cs
@@ -28,16 +28,11 @@ namespace ProtocolAS
      */
     public class Packet
     {
-        public byte magic1 { get; private set; }
-        public byte magic2 { get; private set; }
-        public byte length { get; private set; }
-        public byte cat { get; private set; }
-        public byte cmd { get; private set; }
-        public byte[] payloadn { get; private set; }
-        public byte[] payload2 { get; private set; }
-        public byte checksum1 { get; private set; }
-        public byte checksum2 { get; private set; }
+        public byte magic1 { get { return 0x0E; } }
+        public byte magic2 { get { return 0xE0; } }
         public byte[] newMessage { get; private set; }
+        public int payloadvalue { get; private set; }
+        public byte Length { get; private set; }
 
         byte bitmask = 0xFF;
 
@@ -51,75 +46,44 @@ namespace ProtocolAS
         /// <param name="Payload">Payload - n × 16 bit, Arbitrary number of data bytes carried by the packet.This must be interpreted depending on the command. </param>
         /// <param name="Checksum">Checksum - 16 bits, Checksum of the complete packet computed using the Fletcher-16 algorithm.This is used to detect if the packet was corrupted.</param>
         /// <returns>het gehele bericht</returns>
-        public byte[] Serialize(byte Magic1, byte Magic2, byte Length, byte Cat, byte Cmd, byte[] Payload, byte Checksum1, byte Checksum2)
-        { //maakt payloadarray aan
-            payloadn = new byte[100];
-            for (int i = 0; i < Payload.Length; i++)
+        public byte[] Serialize(byte Command, byte[] Payload, byte Checksum1, byte Checksum2)
+        {
+            if(Payload == null)
             {
-                payloadn[i] = 0x00;
-            };
+                Length = 6;
+            }
+            else
+            {
+                Length = (byte)(6 + Payload.Length);
+            }
+
             //maakt message-array aan
             newMessage = new byte[Length];
             for (int i = 0; i < Length; i++)
             {
                 newMessage[i] = 0x00;
-            };
+            }
 
-            magic1 = Magic1; //1e deel magic
-            magic2 = Magic2; //2e deel magic
-
-            int payloadvalue = Length - 6;  //rekent lengte uit, 6 vanwege 6 bytes kwijt aan Magic, Length, Cat+Cmd, Checksum
-
-            int firstnewbitmask = bitmask & ((byte)Cat << 5); //Cat naar 1e index
-            int secondnewbitmask = bitmask & (Cmd); //cmd naar zijn index
-            int newbitmask = firstnewbitmask | secondnewbitmask;
+            if (Payload != null)
+            {
+                for (int i = 0; i < Payload.Length; i++)//splitst Payload in losse bytes
+                {
+                    newMessage[i + 4] = (byte)(bitmask & (Payload[i]));
+                }
+            }
 
             int check1 = bitmask & (Checksum1); //1e deel checksum
             int check2 = bitmask & (Checksum2); //2e deel checksum
 
-            for (int i = 0; i < payloadvalue; i++)//splitst Payload in losse bytes
-            {
-                payloadn[i] = (byte)(bitmask & (Payload[i] << i * 8));
-            }
             //maakt het bericht vanaf hier
             newMessage[0] = magic1;
             newMessage[1] = magic2;
             newMessage[2] = Length;
-            newMessage[3] = (byte)newbitmask;
-
-            for (int j = 4; j < (payloadvalue + 4) - 1; j++)
-            {
-                newMessage[j] = payloadn[j - 4];
-            }
-            newMessage[payloadvalue - 1] = (byte)check1;
-            newMessage[payloadvalue] = (byte)check2;
+            newMessage[3] = Command;
+            
+            newMessage[Length - 2] = (byte)check1;
+            newMessage[Length - 1] = (byte)check2;
             return newMessage; //verstuurt message, message is klaar
-        }
-
-
-        /// <summary>
-        /// Ontcijfert het bericht dat hij meekrijgt volgens ons protocol
-        /// </summary>
-        /// <param name="message">het bericht dat moet worden ontcijferd</param>
-        public void Deserialize(byte[] message)
-        {
-            magic1 = message[0]; //1e deel magic
-            magic2 = message[1]; //2e deel magic
-
-            length = message[2]; //lengte van bericht
-            int catandcmd = message[3]; //cat en cmd 
-            cat = (byte)(0b11100000 & (catandcmd)); //zet categorie
-            cmd = (byte)(0b00011111 & (message[3])); //zet bericht
-
-            //maakt payloadarray aan
-            payload2 = new byte[100];
-            for (int i = 4; i < length; i++)
-            {
-                payload2[i-4] = message[i]; 
-            }
-
-            checksum1 = message[length - 1]; //maakt in checksum hele verhaal
-            checksum2 = message[length]; //idem
         }
     }
 }
--- ProtocolAS/TestProtocol/TestProtcolAll.cs
+++ ProtocolAS/TestProtocol/TestProtcolAll.cs
@@ -8,64 +8,92 @@ namespace TestProtocol
     public class TestProtcolAll
     {
         Packet p = new Packet();
+        Unpack u = new Unpack();
 
         [TestMethod]
         public void TestSerialize()
         {
-            byte Magic1 = 0x0E;
-            byte Magic2 = 0xE0;
-            byte Length = 0x08;
-            byte Cat = 0xE0;
-            byte Cmd = 0x01;
-            byte[] Payload = new byte[] { 0xA1, 0xA2, 0x33 };
+            byte Command = 0xE1;
+            byte[] Payload = new byte[] { 0xA1, 0xA2, 0x33, 0x44 };
             byte Checksum1 = 0x05;
             byte Checksum2 = 0x30;
-            byte[] Message = new byte[] { 0x0E, 0xE0, 0x08, 0XE1, 0xA1, 0xA2, 0x33, 0x05, 0x30 };
-            byte[] ExpectedMessage = new byte[] { 0x0E, 0xE0, 0x08, 0XE0, 0x01, 0xA1, 0xA2, 0x33, 0x05, 0x30 };
-            byte[] test = p.Serialize(Magic1, Magic2, Length, Cat, Cmd, Payload, Checksum1, Checksum2);
-
+            byte[] Message = new byte[] { 0x0E, 0xE0, 0x0A, 0XE1, 0xA1, 0xA2, 0x33, 0x44, 0x05, 0x30 };
+            byte[] test = p.Serialize(Command, Payload, Checksum1, Checksum2);
+            
+            Assert.AreEqual(4, Payload.Length);
             Assert.AreEqual(Message[0], test[0]);
-            //Assert.AreEqual(Message[1], test[1]);
+            Assert.AreEqual(Message[1], test[1]);
             Assert.AreEqual(Message[2], test.Length);
+            Assert.AreEqual(Message[2], test[2]);
             Assert.AreEqual(Message[3], test[3]);
             Assert.AreEqual(Message[4], test[4]);
-            Assert.AreEqual(Payload[0], test[5]);
-            Assert.AreEqual(Payload[1], test[6]);
-            Assert.AreEqual(Payload[2], test[7]);
-            Assert.AreEqual(Message[8], test[6]);
-            Assert.AreEqual(Message[9], test[7]);
+            Assert.AreEqual(Message[5], test[5]);
+            Assert.AreEqual(Message[6], test[6]);
+            Assert.AreEqual(Message[7], test[7]);
+            Assert.AreEqual(Message[8], test[8]);
+            Assert.AreEqual(Message[9], test[9]);
 
-            /*for(int i = 0; i < test.Length; i++)
+            for(int i = 0; i < test.Length; i++)
             {
                 Assert.AreEqual(Message[i], test[i]);
-            }*/
+            }
+        }
+
+        [TestMethod]
+        public void TestSerializePayloadNull()
+        {
+            byte Command = 0xE1;
+            byte Checksum1 = 0x05;
+            byte Checksum2 = 0x30;
+            byte[] Message = new byte[] { 0x0E, 0xE0, 0x06, 0XE1, 0x05, 0x30 };
+            byte[] test = p.Serialize(Command, null, Checksum1, Checksum2);
+
+            Assert.AreEqual(Message[0], test[0]);
+            Assert.AreEqual(Message[1], test[1]);
+            Assert.AreEqual(Message[2], test.Length);
+            Assert.AreEqual(Message[2], test[2]);
+            Assert.AreEqual(Message[3], test[3]);
+            Assert.AreEqual(Message[4], test[4]);
+            Assert.AreEqual(Message[5], test[5]);
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                Assert.AreEqual(Message[i], test[i]);
+            }
         }
 
         [TestMethod]
         public void TestDeserialize()
         {
-            byte[] Message = new byte[] { 0x0E, 0xE0, 0x08, 0XE1, 0xA1, 0xA2, 0x33, 0x05, 0x30 };
-            p.Deserialize(Message);
-            Assert.AreEqual(0x0E, Message[0]);
-            Assert.AreEqual(0xE0, Message[1]);
-            Assert.AreEqual(0x08, Message[2]);
-            Assert.AreEqual(0XE1, Message[3]);
-            Assert.AreEqual(0xA1, Message[4]);
-            Assert.AreEqual(0xA2, Message[5]);
-            Assert.AreEqual(0x33, Message[6]);
-            Assert.AreEqual(0x05, Message[7]);
-            Assert.AreEqual(0x30, Message[8]);
+            byte[] Message = new byte[] { 0x0E, 0xE0, 0x0A, 0XE1, 0xA1, 0xA2, 0x33, 0x44, 0x05, 0x30 };
+            u.Deserialize(Message);
+           
+            Assert.AreEqual(0x0E, u.magic1);
+            Assert.AreEqual(0xE0, u.magic2);
+            Assert.AreEqual(0x0A, u.length);
+            Assert.AreEqual(0XE0, u.cat);
+            Assert.AreEqual(0x01, u.cmd);
+            Assert.AreEqual(0xA1, u.payload2[0]);
+            Assert.AreEqual(0xA2, u.payload2[1]);
+            Assert.AreEqual(0x33, u.payload2[2]);
+            Assert.AreEqual(0x44, u.payload2[3]);
+            Assert.AreEqual(0x05, u.checksum1);
+            Assert.AreEqual(0x30, u.checksum2);
+        }
 
-            Assert.AreEqual(0x0E, p.magic1);
-            Assert.AreEqual(0xE0, p.magic2);
-            Assert.AreEqual(0x08, p.length);
-            Assert.AreEqual(0XE0, p.cat);
-            Assert.AreEqual(0x01, p.cmd);
-            Assert.AreEqual(0xA1, p.payload2[0]);
-            Assert.AreEqual(0xA2, p.payload2[1]);
-            Assert.AreEqual(0x33, p.payload2[2]);
-            Assert.AreEqual(0x05, p.checksum1);
-            Assert.AreEqual(0x30, p.checksum2);
+        [TestMethod]
+        public void TestDeserializeNoPayload()
+        {
+            byte[] Message = new byte[] { 0x0E, 0xE0, 0x06, 0XE1, 0x05, 0x30 };
+            u.Deserialize(Message);
+
+            Assert.AreEqual(0x0E, u.magic1);
+            Assert.AreEqual(0xE0, u.magic2);
+            Assert.AreEqual(0x06, u.length);
+            Assert.AreEqual(0XE0, u.cat);
+            Assert.AreEqual(0x01, u.cmd);
+            Assert.AreEqual(0x05, u.checksum1);
+            Assert.AreEqual(0x30, u.checksum2);
         }
     }
 }
